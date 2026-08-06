bb148ddc50cdc6e7ea33dfb00052f26407726c14
NGZIYUN <zunne0121@gmail.com>
2026-08-06 23:42:35 +0800

    User class

--- src/User.java
+++ src/User.java
@@ -1,2 +1,34 @@
-public class User {
+import java.util.List;
+import java.util.Objects;
+
+record User(String userName, String password, boolean isAdmin) {
+
+    public boolean isUserAdmin(List<User> userList) {
+
+        boolean found = false;
+        for (User u : userList) {           //userName de getter
+            if (u.isAdmin && Objects.equals(u.userName, userName())) {
+                found = true;
+                break;   // so it won't check the rest of the userList
+            }
+        }
+
+        return found;
+
+    }
+
+    public boolean isPasswordCorrect(List<User> userList) {
+        boolean found = false;
+        for (User u : userList) {           //userName de getter
+            if (Objects.equals(u.password,password()) && Objects.equals(u.userName, userName())) {
+                found = true;
+                break;
+            }
+        }
+
+        return found;
+
+    }
+
+
 }
